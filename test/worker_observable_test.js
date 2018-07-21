0a1cccffc0d69813ffa21bc28245dd9ed25e2773
Matthew Phillips <phillipsm2@gmail.com>
2018-07-21 09:37:09 -0400

    Rename the two primary API methods
    This changes the names of a couple of things:
    
    * proxy -> connect
    * link -> provide

--- test/worker_observable_test.js
+++ test/worker_observable_test.js
@@ -1,6 +1,7 @@
 var canReflect = require("can-reflect");
 var QUnit = require("steal-qunit");
 var workerObservable = require("../can-worker-observable");
+var connect = workerObservable.connect;
 
 function before(hooks, fn) {
 	if(fn.result) return fn.result;
@@ -23,7 +24,7 @@ QUnit.module("basics", function(hooks) {
 	before(hooks, function(assert) {
 		var pth = __dirname + "/../node_modules/steal/steal.js?config=package.json!npm&baseURL=/&main=test/tests/basics/worker";
 		var worker = new Worker(pth);
-		this.api = workerObservable.proxy(worker);
+		this.api = connect(worker);
 	});
 
 	QUnit.module("Constructors", function(){
